5c77a4cb31e822e4c052cb6bf2787cd5fa87f7f1
Jayashree Patnaik <jayashreepatnaik1995@gmail.com>
2025-03-12 00:31:57 +0530

    11th March Task

---
+++ src/ex_01_Java_Basics/NineTable.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basics;
+
+public class NineTable {
+
+    public static void main(String[] args) {
+        int table=9;
+        System.out.printf("9 * 1 = %d\n",(table*1) );
+        System.out.printf("9 * 2 = %d\n",(table*2) );
+        System.out.printf("9 * 3 = %d\n",(table*3) );
+        System.out.printf("9 * 4 = %d\n",(table*4) );
+        System.out.printf("9 * 5 = %d\n",(table*5) );
+        System.out.printf("9 * 6 = %d\n",(table*6) );
+        System.out.printf("9 * 7 = %d\n",(table*7) );
+        System.out.printf("9 * 8 = %d\n",(table*8) );
+        System.out.printf("9 * 9 = %d\n",(table*9) );
+        System.out.printf("9 * 10 = %d\n",(table*10) );
+
+
+    }
+}
